f2d2b99043f11fa1156b006aa5891da542e21630
Weijing Jay lin <weijingjaylin@gmail.com>
2024-04-24 22:29:55 -0700

    add address

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -22,25 +22,42 @@ function CustomizedAvatar({ name, idx }: { name: string; idx: string }) {
 export default function HelpFeedback() {
   return (
     <div>
-      <h1 className={title()}>Contact</h1>
-      <div>
-        {siteConfig.contacts.map((item, idx) => (
-          <Card key={idx} shadow="none">
-            <CardHeader className="flex gap-3">
-              <CustomizedAvatar name={item.name[0]} idx={idx.toString()} />
-              <div className="text-left">
-                <div>{item.name}</div>
-                <div>+86 {item.phone}</div>
-                <div>
-                  <Link href={`mailto:${item.email}`}>{item.email}</Link>
-                </div>
-              </div>
-            </CardHeader>
+      <div className="mb-8">
+        <h1 className={title()}>Contact</h1>
+      </div>
+      <div className="gap-2 grid grid-cols-12">
+        <div className="col-span-12 sm:col-span-6">
+          <h2 className="text-3xl ">Address</h2>
+          <Card shadow="none">
+            <CardBody>
+              <div>202 Shangmei Science and Technology</div>
+              <div>2009 Shahexi Road</div>
+              <div>Shenzhen City</div>
+              <div>Guangdong Province</div>
+              <div>China</div>
+            </CardBody>
           </Card>
-        ))}
-        <Card shadow="none">
-          <CardBody>Beijing Time: 9 am - 6 am</CardBody>
-        </Card>
+        </div>
+        <div className="col-span-12 sm:col-span-6">
+          <h2 className="text-3xl ">Team</h2>
+          {siteConfig.contacts.map((item, idx) => (
+            <Card key={idx} shadow="none">
+              <CardHeader className="flex gap-3">
+                <CustomizedAvatar name={item.name[0]} idx={idx.toString()} />
+                <div className="text-left">
+                  <div>{item.name}</div>
+                  <div>+86 {item.phone}</div>
+                  <div>
+                    <Link href={`mailto:${item.email}`}>{item.email}</Link>
+                  </div>
+                </div>
+              </CardHeader>
+            </Card>
+          ))}
+          <Card shadow="none">
+            <CardBody>Beijing Time: 9 am - 6 am</CardBody>
+          </Card>
+        </div>
       </div>
     </div>
   );
